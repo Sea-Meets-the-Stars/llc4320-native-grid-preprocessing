006edb6ae19c4f92112fd31998418c46b36fa9bc
jaketall7 <jaketall@icloud.com>
2025-12-12 16:15:57 -0800

    working sampling

---
+++ src/data_preprocessing/physical_calculations.py
@@ -0,0 +1,28 @@
+import xarray as xr
+import utils.jmd95_xgcm_implementation as jmd95
+
+def buoyancy_of_field(ds):
+    #ds must have Theta and Salt
+
+    g = 0.0098
+    ref_rho: float = 1025.
+
+    # chunk data
+    ds = ds.chunk({'face': 1, 'j': 720, 'i': 720})
+    p = xr.zeros_like(ds.Theta)  # surface pressure
+
+    rho = xr.apply_ufunc(
+        jmd95.jmd95,
+        ds.Salt,
+        ds.Theta,
+        p,
+        dask="parallelized",
+        output_dtypes=[float],
+    )
+
+    rho = rho.persist()
+
+    buoyancy = g * rho / ref_rho
+    buoyancy = buoyancy.persist()
+
+    return buoyancy
